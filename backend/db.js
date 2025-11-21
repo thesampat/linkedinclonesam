eb8a301bedf40bb5d0e740b01891a01b58df4c92
sampat <sampat.pawar@secquraise.com1>
2025-11-21 09:56:51 +0530

    fixed issue 23 moongo timeout

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,13 @@
 import mongoose from "mongoose";
 
-console.log(process.env.MONGO_URL)
-mongoose.connect(process.env.MONGO_URL)
+mongoose.connect(process.env.MONGO_URL,  {useNewUrlParser: true,   useUnifiedTopology: true,   serverSelectionTimeoutMS: 30000, socketTimeoutMS: 45000})
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
+
+
+mongoose.connection.on('error', err => {
+console.error('Mongoose connection error:', err);
+});
 
 process.on("SIGINT", async () => {
   await mongoose.connection.close();
