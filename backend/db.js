3688906d2eb818e055327f2c38a53dd353fcbea9
sampat <sampat.pawar@secquraise.com1>
2025-11-21 10:44:03 +0530

    update, delete stgtermremoveed

--- backend/db.js
+++ backend/db.js
@@ -15,7 +15,7 @@ process.on("SIGINT", async () => {
   process.exit(0);
 });
 
-process.on("SIGTERM", () => {
-  mongoose.connection.close();
-  console.log("MongoDB disconnected due to SIGTERM");
-});
+// process.on("SIGTERM", () => {
+//   mongoose.connection.close();
+//   console.log("MongoDB disconnected due to SIGTERM");
+// });
